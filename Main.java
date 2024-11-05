0a8a76ae8128d424b9e45f3b6ab831ddb2439970
Btomaszek <bawana2@hawaii.edu>
2024-11-04 23:55:58 -1000

    Update Main.java

--- Main.java
+++ Main.java
@@ -3,6 +3,7 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
+import java.util.Random;
 
 public class Main {
 
@@ -11,10 +12,13 @@ public class Main {
         // Initialize graph and population data using DataLoader.
         Map<String, List<DataLoader.Edge>> graph = DataLoader.getIslandGraph();
         Map<String, Integer> populations = DataLoader.getIslandPopulations();
+        Map<String, Map<String, Integer>> resources = DataLoader.getNaturalResources();
         
         // Initialize variables passed as arguments to algorithms.
         String startIsland;
+        String resource;
         int numCanoes;
+        int canoeCapacity;
 
         // Setup tracking for recency and skills on each island.
         Map<String, Integer> recentVisits = new HashMap<>(); // Last visit timestamp.
@@ -30,7 +34,20 @@ public class Main {
 
         // Display the route taken by the leader for knowledge sharing.
         System.out.println("Leader's Knowledge Sharing Route:");
-        leaderRoute.forEach(System.out::println);
+        leaderRoute.forEach(System.out::println);        
+        
+        //Distribute resources from starting island to others.
+        resource = "Koa Wood";
+        numCanoes = 8;
+        canoeCapacity = 10;
+        List<String> route = Algorithms.distributeResource(graph, resources, startIsland, resource, numCanoes, canoeCapacity);
+        
+        // Ensure the resource is available on the start island. 
+        resources.get(startIsland).put(resource, 100);
+
+        // Display the distribution route. 
+        System.out.println("\nResource Distribution Route:"); 
+        route.forEach(System.out::println);
 
 
         /*
